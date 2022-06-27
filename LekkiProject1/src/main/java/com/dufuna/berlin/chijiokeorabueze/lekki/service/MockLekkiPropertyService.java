66bf4f06504eb48504d59fb3d7a2681449b8361c
ChijiokeOrabueze <95033997+ChijiokeOrabueze@users.noreply.github.com>
2022-06-26 17:28:41 -0700

    updated lekkiservice class to call repository

--- LekkiProject1/src/main/java/com/dufuna/berlin/chijiokeorabueze/lekki/service/MockLekkiPropertyService.java
+++ LekkiProject1/src/main/java/com/dufuna/berlin/chijiokeorabueze/lekki/service/MockLekkiPropertyService.java
@@ -1,18 +1,31 @@
 package com.dufuna.berlin.chijiokeorabueze.lekki.service;
 
 import com.dufuna.berlin.chijiokeorabueze.lekki.model.LekkiProperty;
+import com.dufuna.berlin.chijiokeorabueze.lekki.repository.SimpleLekkiPropertyRepositoryImpl;
 
+import java.util.ArrayList;
 
 
 public class MockLekkiPropertyService implements LekkiPropertyService {
+    SimpleLekkiPropertyRepositoryImpl repo = new SimpleLekkiPropertyRepositoryImpl();
+
     @Override
-    public void saveProperty(LekkiProperty property) {
+    public LekkiProperty saveProperty(LekkiProperty property) {
+        LekkiProperty savedProperty = repo.save(property);
         System.out.println("MockLekkiPropertyService.saveProperty called");
+        return savedProperty;
     }
 
     @Override
-    public LekkiProperty getProperty() {
+    public LekkiProperty getProperty(int id) {
+        LekkiProperty foundProperty = repo.findById(id);
         System.out.println("MockLekkiPropertyService.getProperty called");
-        return null;
+        return foundProperty;
+    }
+
+    @Override
+    public ArrayList getProperties() {
+        ArrayList allProperties = repo.findAll();
+        return allProperties;
     }
 }
